7c65550b97d26ed99db6607fc8dc676102dfb898
Lordbaconbane <wesleychartrand@gmail.com>
2024-08-18 01:04:31 -0400

    Made scraper work for Seraphon specifically

--- pdf_scraper.py
+++ pdf_scraper.py
@@ -1,11 +1,30 @@
+
 import pymupdf
+import requests
 
-order_seraphon_dir = r""
+r = requests.get('https://www.warhammer-community.com/wp-content/uploads/2024/07/FcGNwRXuuaZBJLye.pdf')
+data = r.content
+doc = pymupdf.Document(stream=data)
 
-doc = pymupdf.open(order_seraphon_dir)
+keywords = ["order", "infantry", "hero", "cavalry", "monster", "warmaster", "unique", "beast"]
+filtered_words = []
 
-for page in doc: # iterate the document pages
-  text = page.get_text() # get plain text encoded as UTF-8
+with open("order_seraphon_output.txt", "w", encoding='utf-8') as output:
+  num = 0
+  for page in doc:
+    num = num +1
+    # print(page.number)
+    if page.number >5:
+      text = page.get_text()
+      lines = text.splitlines()
+      bottom_lines = lines[-3:]
+      for line in bottom_lines:
+        line = line.lower()
+        print(line)
+        for keyword in keywords:
+          if keyword in line:
+            filtered_words.append(line)
+          
   
-  with open("order_seraphon_output.txt", "a", encoding='utf-8') as output:
-    output.write(text)
+  for word in filtered_words:
+    output.write(word + "\n")    
